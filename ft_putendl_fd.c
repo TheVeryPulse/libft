6df68e4bfa419949cd6c26fee799ea3d9b1610b9
Philip Li 42 <macpilot23@outlook.com>
2023-12-14 14:20:18 +0000

    ft_putendl_fd update
    Copy the string and add \n before calling write()

--- ft_putendl_fd.c
+++ ft_putendl_fd.c
@@ -3,23 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putendl_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juli <juli@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 10:26:30 by juli              #+#    #+#             */
-/*   Updated: 2023/12/13 15:37:20 by juli             ###   ########.fr       */
+/*   Updated: 2023/12/14 14:09:04 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "libft.h"
 
 /* Outputs the string 's' to the given file descriptor followed by a newline */
 void	ft_putendl_fd(char *s, int fd)
 {
+	char	*to_write;
+	size_t	i;
 	size_t	len;
 
-	len = 0;
-	while (s[len])
-		len++;
-	write(fd, s, len);
-	write(fd, "\n", 1);
+	len = ft_strlen(s);
+	to_write = (char *)malloc(len + 1);
+	if (!to_write)
+		return ;
+	i = 0;
+	while (i < len)
+	{
+		to_write[i] = s[i];
+		i++;
+	}
+	to_write[i++] = '\n';
+	write(fd, to_write, len + 1);
+	free(to_write);
 }
