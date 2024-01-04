be5451b1d917e91e48df2af935f5ea2118c1f1c1
Philip Li 42 <macpilot23@outlook.com>
2024-01-04 13:08:51 +0000

    Folder update

--- Makefile
+++ Makefile
@@ -1,54 +1,58 @@
 CC := gcc
-CFLAGS := -Wall -Wextra -Werror
-CFILES_LIBFT := \
-	ft_isalpha.c \
-	ft_isdigit.c \
-	ft_isalnum.c \
-	ft_isascii.c \
-	ft_isspace.c \
-	ft_isprint.c \
-	ft_isupper.c \
-	ft_islower.c \
-	ft_strlen.c \
-	ft_memset.c \
+SRC_DIR := ./src
+INC_DIR := ./inc
+LIB_DIR := ./lib
+BIN_DIR := ./bin
+
+CFLAGS := -Wall -Wextra -Werror -I $(INC_DIR)
+FILE_NAMES := \
+	ft_atoi.c \
 	ft_bzero.c \
-	ft_memcpy.c \
-	ft_memmove.c \
-	ft_strlcpy.c \
-	ft_strlcat.c \
-	ft_toupper.c \
-	ft_tolower.c \
-	ft_strchr.c \
-	ft_strrchr.c \
-	ft_strncmp.c \
+	ft_calloc.c \
+	ft_isalnum.c \
+	ft_isalpha.c \
+	ft_isascii.c \
+	ft_isdigit.c \
+	ft_islower.c \
+	ft_isprint.c \
+	ft_isspace.c \
+	ft_isupper.c \
+	ft_itoa.c \
+	ft_lstadd_front.c \
+	ft_lstadd_back.c \
+	ft_lstclear.c \
+	ft_lstdelone.c \
+	ft_lstiter.c \
+	ft_lstlast.c \
+	ft_lstmap.c \
+	ft_lstnew.c \
+	ft_lstsize.c \
 	ft_memchr.c \
 	ft_memcmp.c \
-	ft_strnstr.c \
-	ft_atoi.c \
-	ft_calloc.c \
-	ft_strdup.c \
-	ft_substr.c \
-	ft_strjoin.c \
-	ft_strtrim.c \
-	ft_split.c \
-	ft_itoa.c \
-	ft_strmapi.c \
-	ft_striteri.c \
+	ft_memcpy.c \
+	ft_memmove.c \
+	ft_memset.c \
 	ft_putchar_fd.c \
-	ft_putstr_fd.c \
 	ft_putendl_fd.c \
 	ft_putnbr_fd.c \
-	ft_lstnew.c \
-	ft_lstadd_front.c \
-	ft_lstsize.c \
-	ft_lstlast.c \
-	ft_lstadd_back.c \
-	ft_lstdelone.c \
-	ft_lstclear.c \
-	ft_lstiter.c \
-	ft_lstmap.c \
-
-CFILES_PRINTF := \
+	ft_putstr_fd.c \
+	ft_split.c \
+	ft_strchr.c \
+	ft_strdup.c \
+	ft_striteri.c \
+	ft_strjoin.c \
+	ft_strlcat.c \
+	ft_strlcpy.c \
+	ft_strlen.c \
+	ft_strmapi.c \
+	ft_strncmp.c \
+	ft_strnstr.c \
+	ft_strrchr.c \
+	ft_strtrim.c \
+	ft_substr.c \
+	ft_tolower.c \
+	ft_toupper.c \
+	\
 	ft_printf.c \
 	ft_printf_all.c \
 	ft_printf_c.c \
@@ -57,31 +61,29 @@ CFILES_PRINTF := \
 	ft_printf_d.c \
 	ft_printf_u.c \
 	ft_printf_x.c \
-	ft_abs.c
-
-CFILES_GNL := \
+	ft_abs.c \
+	\
 	get_next_line.c \
-	get_next_line_utils.c \
+	get_next_line_utils.c
+SRC_FILES := $(addprefix $(SRC_DIR)/, $(FILE_NAMES))
+OBJ_FILES := $(patsubst $(SRC_DIR)/%.c, $(BIN_DIR)/%.o, $(SRC_FILES))
 
-OFILES_LIBFT := $(CFILES_LIBFT:.c=.o)
-OFILES_PRINTF := $(CFILES_PRINTF:.c=.o)
-OFILES_GNL := $(CFILES_GNL:.c=.o)
-NAME := libft.a
+NAME := $(LIB_DIR)/libft.a
 
 # build object files and archive the static library
 all: $(NAME)
 
 # build the library with the required .o files
-$(NAME): $(OFILES_LIBFT) $(OFILES_PRINTF) $(OFILES_GNL)
+$(NAME): $(OBJ_FILES)
 	ar rcs $@ $^
 
 # build .o files from .c files
-%.o: %.c
+$(BIN_DIR)/%.o: $(SRC_DIR)/%.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
 # remove all .o files
 clean:
-	rm -f *.o
+	rm -f $(BIN_DIR)/*.o
 
 # remove static library as well as all .o files
 fclean: clean
@@ -92,9 +94,5 @@ re: fclean all
 
 # compile everything 
 bonus: $(NAME) clean
-
-# print information on all .c files
-print: $(wildcard *.c)
-	ls -la $(wildcard *.c)
 
 .PHONY: all clean fclean re
--- inc/libft.h
+++ inc/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juli <juli@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 20:03:04 by juli              #+#    #+#             */
-/*   Updated: 2023/12/13 16:04:49 by juli             ###   ########.fr       */
+/*   Updated: 2024/01/04 12:45:05 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,79 +22,44 @@
 /* basic functions */
 
 int		ft_isalpha(int c);
-
 int		ft_isdigit(int c);
-
 int		ft_isalnum(int c);
-
 int		ft_isascii(int c);
-
 int		ft_isspace(int c);
-
 int		ft_isprint(int c);
-
 int		ft_isupper(int c);
-
 int		ft_islower(int c);
-
 size_t	ft_strlen(const char *s);
-
 void	*ft_memset(void *s, int c, size_t n);
-
 void	ft_bzero(void *s, size_t n);
-
 void	*ft_memcpy(void *dest, const void *src, size_t n);
-
 void	*ft_memmove(void *dest, const void *src, size_t n);
-
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-
 int		ft_toupper(int c);
-
 int		ft_tolower(int c);
-
 char	*ft_strchr(const char *s, int c);
-
 char	*ft_strrchr(const char *s, int c);
-
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-
 void	*ft_memchr(const void *s, int c, size_t n);
-
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
-
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-
 int		ft_atoi(const char *nptr);
-
 void	*ft_calloc(size_t nmemb, size_t size);
-
 char	*ft_strdup(const char *s);
 
 /* additional functions */
 
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-
 char	*ft_strjoin(char const *s1, char const *s2);
-
 char	*ft_strtrim(char const *s1, char const *set);
-
 char	**ft_split(char const *s, char c);
-
 char	*ft_itoa(int n);
-
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
-
 void	ft_putchar_fd(char c, int fd);
-
 void	ft_putstr_fd(char *s, int fd);
-
 void	ft_putendl_fd(char *s, int fd);
-
 void	ft_putnbr_fd(int n, int fd);
 
 /* Linked List */
@@ -106,41 +71,25 @@ typedef struct s_list
 }					t_list;
 
 t_list	*ft_lstnew(void *content);
-
 void	ft_lstadd_front(t_list **lst, t_list *new);
-
 int		ft_lstsize(t_list *lst);
-
 t_list	*ft_lstlast(t_list *lst);
-
 void	ft_lstadd_back(t_list **lst, t_list *new);
-
 void	ft_lstdelone(t_list *lst, void (*del)(void *));
-
 void	ft_lstclear(t_list **lst, void (*del)(void *));
-
 void	ft_lstiter(t_list *lst, void (*f)(void *));
-
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 /* ft_printf */
 
 int		ft_printf(const char *str, ...);
-
 int		ft_printf_all(va_list args, int str_i);
-
 int		ft_printf_c(char c);
-
 int		ft_printf_s(char *s);
-
 int		ft_printf_p(void *p);
-
 int		ft_printf_d(int n);
-
 int		ft_printf_u(unsigned int n);
-
 int		ft_printf_x(unsigned int n, int letter_case);
-
 int		ft_abs(int n);
 
 /* get_next_line */
@@ -163,19 +112,12 @@ typedef struct s_str_list
 }	t_str_list;
 
 char	*get_next_line(int fd);
-
 void	_cut_line_into_buffer(char *fd_buffer, char *tmp_buffer);
-
 void	_save_to_fd_buffer(char **fd_buffer, char *source);
-
 void	_add_until_eol_to_list(t_str_list **head, char *buffer);
-
 char	*_list_to_line(t_str_list *list);
-
 int		_total_strlen_from_list(t_str_list *list);
-
 void	_free_and_set_to_null(t_str_list **list, char **buffer);
-
 void	_read_until_eol_or_eof_and_save_excess(int fd, char *tmp_buffer,
 			t_str_list **head, char **fd_buffer);
 
