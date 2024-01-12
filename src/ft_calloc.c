e6fcfafb96d3795c0f291e14050e1b113898c670
Philip Li 42 <macpilot23@outlook.com>
2024-01-12 23:10:41 +0000

    comment doc

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -3,15 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juli <juli@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 18:42:36 by juli              #+#    #+#             */
-/*   Updated: 2023/12/13 15:30:43 by juli             ###   ########.fr       */
+/*   Updated: 2024/01/11 23:24:19 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
+/**
+ * @brief
+ * Allocates memory for an array of 'nmemb' elements of 'size' bytes each and
+ * returns a pointer to the allocated memory. The memory is set to zero.
+ *
+ * Checks for overflow when calculating the total size of memory to allocate.
+ *
+ * @param nmemb Number of elements to be allocated.
+ * @param size Size of each element.
+ * @return void*
+ * A pointer to the allocated memory initialized to zero. 
+ * If allocation fails, or if overflow occurs when calculating the total size,
+ * the function returns NULL.
+ * If 'nmemb' or 'size' is 0, the function returns a unique pointer that can be
+ * successfully passed to free(). */
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	char	*ary;
