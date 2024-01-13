3dfbc3ba55720629483eed31b920f17d88638030
Philip Li <macpilot23@outlook.com>
2024-01-13 02:20:34 +0000

    Comment

--- src/ft_bzero.c
+++ src/ft_bzero.c
@@ -3,15 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: juli <juli@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: Philip <juli@student.42london.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/30 17:56:22 by juli              #+#    #+#             */
-/*   Updated: 2023/12/13 16:07:21 by juli             ###   ########.fr       */
+/*   Created: 2023/10/30 17:56:22 by Philip            #+#    #+#             */
+/*   Updated: 2024/01/13 02:12:55 by Philip           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
+/**
+ * @brief Sets the first n bytes of the block of memory pointed by s to zero.
+ * 
+ * @param s Pointer to the block of memory to fill.
+ * @param n Number of bytes to set to zero.
+ */
 void	ft_bzero(void *s, size_t n)
 {
 	size_t	i;
--- src/ft_isspace.c
+++ src/ft_isspace.c
@@ -10,6 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @brief Checks if the 'c' is a white-space character.
+ * 
+ * White-space characters are: ' ', '\\t', '\\n', '\\v', '\\f', and '\\r'.
+ * 
+ * @param c Character to check.
+ * @return int 
+ * Non-zero if the character is a white-space character, and zero otherwise.
+ */
 int	ft_isspace(int c)
 {
 	return ((c >= '\t' && c <= '\r') || c == ' ');
